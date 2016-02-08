733472030d9101febb1878003af185099f093a6e
Bhagyashri Pachkor <bhagyashrip547@gmail.com>
2016-02-08 21:09:05 +0530

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,20 @@
+
+public class Item {
+  private String productName;
+  private int quantity;
+  private double unitPrice;
+  public Item (String productName, int quantity, double unitPrice ) {
+    this.productName = productName;
+    this.quantity = quantity;
+    this.unitPrice = unitPrice;
+  }
+  public String getProductName() {
+    return this.productName;
+  }
+  public int getQuantity() {
+    return this.quantity;
+  }
+  public double getUnitPrice() {
+    return this.unitPrice;
+  }
+}
